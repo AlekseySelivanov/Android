c9602b52c38de27a933ad4650fb11642b64c3fdb
AlekseySelivanov <salex92@mail.ru>
2021-01-25 17:43:23 +0300

    HW6

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,36 +1,34 @@
 package com.example.myapplication;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, IRVOnItemClick {
     Button button4;
     Button button;
-    Button button3;
     String LOG = "log";
+    private RecyclerView recyclerView;
+    private RecyclerDataAdapter adapter;
+
+private ArrayList<String> listData = new ArrayList<>(Arrays.asList("Понедельник 15С", "Вторник 15С","Среда 15С","Четверг 15С","Пятница 15С","Суббота 15С","Воскресенье 15С"));
+
+
+
 
 
 
@@ -42,67 +40,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
         String value= getIntent().getStringExtra("getData");
         ((TextView)findViewById(R.id.textView7)).setText(value);
-
 
         button = (Button) findViewById(R.id.button);
         button.setOnClickListener(this);
         button4 = (Button) findViewById(R.id.button4);
         button4.setOnClickListener(this);
-        button3 = (Button) findViewById(R.id.button3);
-        button3.setOnClickListener(this);
+
+        initViews();
+        setupRecyclerView();
+    }
+
+    private void initViews(){
+        recyclerView = findViewById(R.id.recyclerView);
+    }
+    private void setupRecyclerView() {
+        LinearLayoutManager layoutManager = new LinearLayoutManager(
+                getBaseContext());
+        adapter = new RecyclerDataAdapter(listData, this);
+
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
 
 
 
     }
 
-
-
     @Override
-    protected void onStart() {
-        Log.d(LOG, "onStart");
-        super.onStart();
-
-    }
-
-    @Override
-    protected void onResume() {
-        Log.d(LOG, "onResume");
-        super.onResume();
-
-    }
-
-    @Override
-    protected void onPause() {
-        Log.d(LOG, "onPause");
-        super.onPause();
-
-    }
-
-    @Override
-    protected void onStop() {
-        Log.d(LOG, "onStop");
-        super.onStop();
-
-
-    }
-
-    @Override
-    protected void onRestart() {
-        Log.d(LOG, "onRestart");
-        super.onRestart();
-
-
-    }
-
-    @Override
-    protected void onDestroy() {
-        Log.d(LOG, "onDestroy");
-        super.onDestroy();
-
+    public void onItemClicked(String itemText) {
+        Toast.makeText(getBaseContext(), itemText, Toast.LENGTH_SHORT).show();
     }
 
     @SuppressLint("NonConstantResourceId")
@@ -117,13 +84,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Intent intent1 = new Intent(this, Settings.class);
                 startActivity(intent1);
                 break;
-            case R.id.button3:
-                Fragment frag = new FragmentEmblem();
-                FragmentManager manager = getSupportFragmentManager();
-                FragmentTransaction transaction = manager.beginTransaction();
-                transaction.replace(R.id.fragment_frame,frag,"Test Fragment");
-                transaction.addToBackStack(null);
-                transaction.commit();
             default:
                 break;
         }
